6d6ebcbc399d0df2d2425577ed05a2876920c5a5
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-09-11 16:38:49 +0200

    provo a stampare i post in pagina

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/posts/';
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+  components: { AppHeader },
+  data: () => ({ posts: [] }),
+  methods: {
+    fetchPosts() {
+      axios.get(endpoint).then(res => { console.log(res.data) })
+    }
+  },
+  created() {
+    this.fetchPosts();
+  }
 };
 </script>
 
 <template>
-  <h1>Hello!</h1>
+  <AppHeader />
+  <main class="container my-5">
+    <h1>Hello!</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </main>
 </template>
 
 <style></style>
